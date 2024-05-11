a333fb574167bd41853c2f687eacc21a06dc4dc4
Diego Arriola <diegovargaskolo@gmail.com>
2024-05-11 02:15:29 -0600

    cambios

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -25,10 +26,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // INDEX ROUTE 
-app.get("/", function(req,res)
-{
-  res.render("index", {title:"Home"} )
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information

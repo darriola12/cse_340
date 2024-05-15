7c9b231adec11b527374df42b834c240a69850d8
Diego Arriola <diegovargaskolo@gmail.com>
2024-05-15 16:32:27 -0600

    cambios

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
 const error = require("./routes/error")
+const session = require("express-session")
+const pool = require('./database/')
+const account = require("./routes/accountRoute")
 
 
 /* ***********************
@@ -25,6 +28,28 @@ app.set("layout", "./layouts/layout") // not at views root
 
 
 /* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
@@ -32,10 +57,14 @@ app.use(static)
 // INDEX ROUTE 
 app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", inventoryRoute)
+app.use("/account", require("./routes/accountRoute"))
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 app.use("/", error)
+
+
+
 
 
 
